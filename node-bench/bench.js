db3d433c2b8ee3ce479966af7c00849d8c0bc632
Maciej Hirsz <maciej.hirsz@gmail.com>
2016-07-01 08:12:27 +0200

    Fix up the Node.js benches

--- node-bench/bench.js
+++ node-bench/bench.js
@@ -1,16 +1,18 @@
 let json = `{"timestamp":2837513946597,"zone_id":123456,"zone_plan":1,"http":{"protocol":2,"status":200,"host_status":503,"up_status":520,"method":1,"content_type":"text/html","user_agent":"Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/33.0.1750.146 Safari/537.36","referer":"https://www.cloudflare.com/","request_uri":"/cdn-cgi/trace"},"origin":{"ip":"1.2.3.4","port":8000,"hostname":"www.example.com","protocol":2},"country":238,"cache_status":3,"server_ip":"192.168.1.1","server_name":"metal.cloudflare.com","remote_ip":"10.1.2.3","bytes_dlv":123456,"ray_id":"10c73629cce30078-LAX"}`;
 let data = JSON.parse(json);
 const bytes = json.length;
-const maxIterations = 1;
 
-function bench(iter) {
+function bench(maxIterations, iter) {
     let iterations = maxIterations;
 
     const start = Date.now();
     let totalNanos = 0;
     while (iterations--) {
-        // data = JSON.parse(json);
-        // json = JSON.stringify(data);
+
+        // Re-create the references to pretend it's new data
+        data = JSON.parse(json);
+        json = JSON.stringify(data);
+
         const start = process.hrtime();
         iter();
         totalNanos += process.hrtime(start)[1];
@@ -26,10 +28,20 @@ function bench(iter) {
     console.log('');
 }
 
-bench(function parse() {
+
+bench(1, function parse_cold() {
     out = JSON.parse(json);
 });
 
-bench(function serialize() {
+
+bench(1, function serialize_cold() {
+    out = JSON.parse(json);
+});
+
+bench(100000, function parse_hot() {
+    out = JSON.parse(json);
+});
+
+bench(100000, function serialize_hot() {
     out = JSON.stringify(data);
 });
